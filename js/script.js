82204dec38b95ef1f6c4f14154883e414df82281
MontherSaiid <saeedmundher@gmail.com>
2022-07-27 13:33:23 +0200

    update
    add some properties

--- js/script.js
+++ js/script.js
@@ -3,18 +3,22 @@ let spacing = document.getElementById("spacing");
 let blur = document.getElementById("blur");
 let bgColor = document.getElementById("base");
 let image = document.getElementById("image");
+let rootEle = document.querySelector(":root");
 
 // change the width of image
 spacing.addEventListener("change", function () {
-    image.style.width = (spacing.value * 5) + "px";
+    // image.style.width = (spacing.value * 5) + "px";
+    rootEle.style.setProperty("--wd-image", (spacing.value * 5) + "px");
+
 });
 
 // change the blur of image
 blur.addEventListener("change", function () {
-    image.style.filter = "blur(" + blur.value + "px)";
+    // image.style.filter = "blur(" + blur.value + "px)";
+    rootEle.style.setProperty("--img-blur", blur.value + "px");
 });
 
 // change the border,of image
 bgColor.addEventListener("change", function () {
-    image.style.border = "30px solid " + bgColor.value;
+    rootEle.style.setProperty("--clr-bg", bgColor.value);
 });
